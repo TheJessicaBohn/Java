7e82dd0a6d1bfe4fd0870e35389c54888a011c48
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-10-14 15:41:48 -0300

    PromocaoAutomatica

---
+++ Exercicios/PromocaoAutomatica.java
@@ -0,0 +1,40 @@
+public class PromocaoAutomatica {
+	public static void main(String args[]) {
+		byte a = 10;
+		short b = 20;
+		char c = 'J';
+		int d = 30;
+		long e = 40L;
+		float f = 1.5f;
+		double g = 2.5;
+
+		System.out.println("Valor do char: " + c);
+
+		System.out.println("Valor da soma de a + b = " + (a+b));
+		System.out.println("Valor da soma de a + c = " + (a+c)); // tabela ask promovida para o valor somado
+		System.out.println("Valor da soma de a + d = " + (a+d));
+		System.out.println("Valor da soma de a + e = " + (a+e));
+		System.out.println("Valor da soma de a + f = " + (a+f));
+		System.out.println("Valor da soma de a + g = " + (a+g));
+
+		System.out.println("Valor da soma de b + c = " + (b+c));
+		System.out.println("Valor da soma de b + d = " + (b+d));
+		System.out.println("Valor da soma de b + e = " + (b+e));
+		System.out.println("Valor da soma de b + f = " + (b+f));
+		System.out.println("Valor da soma de b + g = " + (b+g));
+
+		System.out.println("Valor da soma de c + d = " + (c+d));
+		System.out.println("Valor da soma de c + e = " + (c+e));
+		System.out.println("Valor da soma de c + f = " + (c+f));
+		System.out.println("Valor da soma de c + g = " + (c+g));
+
+		System.out.println("Valor da soma de d + e = " + (d+e));
+		System.out.println("Valor da soma de d + f = " + (d+f));
+		System.out.println("Valor da soma de d + g = " + (d+g));
+
+		System.out.println("Valor da soma de e + f = " + (e+f));
+		System.out.println("Valor da soma de e + g = " + (e+g));
+
+		System.out.println("Valor da soma de f + g = " + (f+g));
+	}
+}
