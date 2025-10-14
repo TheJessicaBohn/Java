215a50a832ff441d9ec3d909abb7c097339483d1
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-10-14 12:45:26 -0300

    Create Comentarios.java

---
+++ Exemplos/Comentarios.java
@@ -0,0 +1,16 @@
+public class Comentarios {
+	public static void main(String args[]) {
+
+		// Este é um comentário de linha única
+
+		  /*
+			  Este é um comentário
+			  de várias linhas
+		  */
+
+		 /**
+		 	* Este é um comentário Javadoc
+		 */
+			
+	}
+}
