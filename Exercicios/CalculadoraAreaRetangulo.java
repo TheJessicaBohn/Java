38134aad1ed0f05d8fd752d464613e3de583eb15
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-10-14 15:05:49 -0300

    Cálculo da Área de um Retângulo

---
+++ Exercicios/CalculadoraAreaRetangulo.java
@@ -0,0 +1,11 @@
+	public class CalculadoraAreaRetangulo {
+		public static void main (String args[]) {
+
+			double largura = 5.5;
+			double altura = 10.0;
+			double areaRetangulo = largura * altura;
+
+			System.out.println("Area do Reangulo: " + areaRetangulo);	
+
+		}
+	}
