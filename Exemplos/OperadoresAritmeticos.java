2b2f8b7738622848ec2b22fe87185ab812d685b0
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-10-17 11:18:28 -0300

    Create OperadoresAritmeticos.java

---
+++ Exemplos/OperadoresAritmeticos.java
@@ -0,0 +1,16 @@
+public class OperadoresAritmeticos {
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 2;
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("valor a + b = " + (a + b)); // Saída: 12
+		System.out.println("valor a - b = " + (a - b)); // Saída: 8
+		System.out.println("valor a * b = " + (a * b)); // Saída: 20
+		System.out.println("valor a / b = " + (a / b)); // Saída: 5
+		System.out.println("valor a % b = " + (a % b)); // Saída: 0
+
+	}
+}
