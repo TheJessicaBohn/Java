0cd458526372caf4d86fab2a6bd61e44fdddead0
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-11-03 16:17:45 -0300

    Create OperadoresAtribuicao.java

---
+++ Exemplos/OperadoresAtribuicao.java
@@ -0,0 +1,12 @@
+public class OperadoresAtribuicao {
+	public static void main(String[] args) {
+
+	  int a = 10;
+	  a += 2; // Equivalente a a = a + 2;
+	  System.out.println(a); // Saída: 12
+
+	  a -= 4;
+	  System.out.println(a); // Saída: 8
+
+	}
+}
