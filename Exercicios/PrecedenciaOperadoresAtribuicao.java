d169edec01f0de518a059b15d12d9c5fe7b933ba
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-11-04 14:37:03 -0300

    Update PrecedenciaOperadoresAtribuicao.java

--- Exercicios/PrecedenciaOperadoresAtribuicao.java
+++ Exercicios/PrecedenciaOperadoresAtribuicao.java
@@ -1,13 +1,17 @@
 public class PrecedenciaOperadoresAtribuicao {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
+	int numero = 5;
 
-		int numero = 5;
-		System.out.println("numero: " + numero );
-		
-		numero ++;
-		System.out.println("numero++: " + (numero));
-		
-		numero--;
-		System.out.println("numero--: " + (numero));
+	System.out.println("Pré-incremento (++numero): " + (++numero)); 
+
+	System.out.println("Pós-incremento (numero++): " + (numero++));
+	
+	System.out.println("Valor após o pós-incremento: " + numero);
+
+	System.out.println("Pré-decremento (--numero): " + (--numero));
+
+	System.out.println("Pós-decremento (numero--): " + (numero--));
+	
+	System.out.println("Valor após o pós-decremento: " + numero);
 	}
 }
