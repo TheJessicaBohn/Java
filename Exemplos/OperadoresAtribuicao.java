b87d728b3c4fde2b5009c6d9ebd3fe423a2c0cf7
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-11-04 09:18:05 -0300

    Update OperadoresAtribuicao.java

--- Exemplos/OperadoresAtribuicao.java
+++ Exemplos/OperadoresAtribuicao.java
@@ -1,12 +1,9 @@
 public class OperadoresAtribuicao {
 	public static void main(String[] args) {
 
-	  int a = 10;
-	  a += 2; // Equivalente a a = a + 2;
-	  System.out.println(a); // Saída: 12
-
-	  a -= 4;
-	  System.out.println(a); // Saída: 8
-
+		 int a = 10;
+		 System.out.println("a = " + a);
+		 a += 2; // Equivalente a a = a + 2;
+		 System.out.println("a atribuido:"+ a); // Saída: 12
 	}
 }
