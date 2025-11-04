9b193c3726a0de925c90cff6b4a188143aea3baf
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-11-04 09:25:52 -0300

    Create CalculadoraSimples.java

---
+++ Exercicios/CalculadoraSimples.java
@@ -0,0 +1,13 @@
+public class CalculadoraSimples {
+	public static void main(String args[]){
+
+		var a = 10;
+		var b = 2;
+
+		System.out.println("Soma de a + b = "+ (a+b));
+		System.out.println("Subitração de a - b = "+ (a-b));
+		System.out.println("Multiplicação  de a * b = "+ (a*b));
+		System.out.println("Divisão de a / b = "+ (a/b));
+		System.out.println("Resto da divisão de a % b = "+ (a%b));
+	}
+}
