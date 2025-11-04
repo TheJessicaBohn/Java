29f306d22823092d72b72413d9b510bc125b2a84
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-11-04 11:13:31 -0300

    Create OperadoresAtribuicao.java

---
+++ Exercicios/OperadoresAtribuicao.java
@@ -0,0 +1,12 @@
+public class OperadoresAtribuicao {
+	public static void main(String args[]) {
+
+		int numero = 5;
+
+		System.out.println("Valor de numero: " + (numero));
+		System.out.println("+= " + (numero+=3));
+		System.out.println("-= " + (numero-=2));
+		System.out.println("*= " + (numero*=4));
+		System.out.println("/= " + (numero/=2));
+	}
+}
