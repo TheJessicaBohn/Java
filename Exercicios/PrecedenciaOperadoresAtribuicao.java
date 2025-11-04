b93cbff7f9be83dbe59adb975ae8d737e3df4d36
Jéssica Bohn <47541659+TheJessicaBohn@users.noreply.github.com>
2025-11-04 12:08:32 -0300

    Create PrecedenciaOperadoresAtribuicao.java

---
+++ Exercicios/PrecedenciaOperadoresAtribuicao.java
@@ -0,0 +1,13 @@
+public class PrecedenciaOperadoresAtribuicao {
+	public static void main(String args[]) {
+
+		int numero = 5;
+		System.out.println("numero: " + numero );
+		
+		numero ++;
+		System.out.println("numero++: " + (numero));
+		
+		numero--;
+		System.out.println("numero--: " + (numero));
+	}
+}
